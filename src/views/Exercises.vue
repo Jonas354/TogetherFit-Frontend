54f6c30b3914c4e0a9e599bbe007aa5b91366a31
Jonas Render <jonasrender@icloud.com>
2021-12-15 18:47:09 +0100

    exercise table

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>Welcome this is a test!</h1>
+  <h1>Welcome to Exercises!</h1>
+  <table class="table">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr class="col" v-for="exercise in exercises" :key="exercise.id">
+        <th scope="row">{{exercise.id}}</th>
+        <td>{{exercise.name}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
   name: 'Exercises',
+  data() {
+    return {
+      exercises: [
+        {
+          id: 1,
+          name: 'curls',
+        },
+        {
+          id: 2,
+          name: 'situps',
+        },
+      ],
+    };
+  },
 };
 </script>
 
