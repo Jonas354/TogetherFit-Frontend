6e61c5924b04e6d59fed41969329b0e9c04da29d
MartiK4 <martinkffo@web.de>
2021-12-28 16:14:25 +0100

    input form component

---
+++ src/components/CreateExercise.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="exerciseName" class="form-label">Exercise Name</label>
+      <input type="text" class="form-control" id="exerciseName" v-model="exerciseName">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CreateExercise',
+  data() {
+    return {
+      exerciseName: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
