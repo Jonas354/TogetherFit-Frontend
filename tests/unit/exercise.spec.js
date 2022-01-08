4f8812fdfa53b4ce8eb94cc6f22a15f31a2eceff
MartiK4 <martinkffo@web.de>
2022-01-08 12:04:54 +0100

    exercise component tests

--- tests/unit/exercise.spec.js
+++ tests/unit/exercise.spec.js
@@ -1,14 +1,23 @@
 import { mount } from '@vue/test-utils';
 import Exercises from '@/views/Exercises.vue';
 import CreateExercise from '@/components/CreateExercise.vue';
+import ExerciseTable from '@/components/ExerciseTable.vue';
 
 describe('Exercises.vue', () => {
-  it('should have component', () => {
+  it('should have component CreateExercise component', () => {
     // when
     const wrapper = mount(Exercises);
 
     // then
     const exerciseFrom = wrapper.findComponent(CreateExercise);
     expect(exerciseFrom.exists()).toBeTruthy();
+  });
+  it('should have ExerciseTable component', () => {
+    // when
+    const wrapper = mount(Exercises);
+
+    // then
+    const exerciseTable = wrapper.findComponent(ExerciseTable);
+    expect(exerciseTable.exists()).toBeTruthy();
   });
 });
